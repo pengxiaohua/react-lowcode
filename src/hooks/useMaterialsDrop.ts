7ce19c3c1d73eba924722130153f0f15fe7fc607
Xiaohua Peng <609370075@qq.com>
2024-10-07 23:54:16 +0800

    feat: add drag and delete

--- src/hooks/useMaterialsDrop.ts
+++ src/hooks/useMaterialsDrop.ts
@@ -1,35 +1,50 @@
 import { useDrop } from 'react-dnd';
 
-import { useComponentsStore } from '../stores/components';
+import { getComponentById, useComponentsStore } from '../stores/components';
 import { useComponentConfigStore } from '../stores/component-config';
 
+interface IItemType {
+  type: string;
+  dragType?: 'move' | 'add';
+  id: number;
+}
+
 const useMaterialsDrop = (accept: string[], id: number) => {
-  const { addComponent } = useComponentsStore();
+  const { addComponent, components, removeComponent } = useComponentsStore();
   const { componentConfig } = useComponentConfigStore();
 
   const [{ canDrop }, drop] = useDrop(() => ({
     accept,
-    drop: (item: { type: string }, monitor) => {
+    drop: (item: IItemType, monitor) => {
       // 当组件被拖拽后，如果组件被放置到了该区域，则执行该方法
       const didDrop = monitor.didDrop();
       if (didDrop) {
         return;
       }
+      console.log({ item });
 
-      const config = componentConfig[item.type];
+      if (item.dragType === 'move') {
+        const component = getComponentById(item.id, components)!;
 
-      addComponent(
-        {
-          id: new Date().getTime(),
-          name: item.type,
-          desc: config.desc,
-          props: config.defaultProps,
-          styles: {
-            // background: 'green',
+        removeComponent(item.id);
+
+        addComponent(component, id);
+      } else {
+        const config = componentConfig[item.type];
+
+        addComponent(
+          {
+            id: new Date().getTime(),
+            name: item.type,
+            desc: config.desc,
+            props: config.defaultProps,
+            styles: {
+              // background: 'green',
+            },
           },
-        },
-        id
-      );
+          id
+        );
+      }
     },
     collect: (monitor) => ({
       canDrop: monitor.canDrop(),
